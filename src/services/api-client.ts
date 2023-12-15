5d43fdabaa7b3a50dfe18645a1470321faa16970
luv nft <info@blkluv.org>
2023-12-15 06:39:34 -0500

    updated api

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api/',
     params: {
-        key: 'eefdf34748944a10880c865df9404fd9'
+        key: 'e0f96a50c5e647059c75f5a9f880e43c'
     }
 });
